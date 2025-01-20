456e9b7c6296cb67e387bee3838b1c814e887762
ryusei-d <dateryuusei@dateryuuseinoMacBook-Air.local>
2025-01-20 20:27:18 +0900

    ecsite

---
+++ src/main/java/jp/co/internous/ecsite/model/mapper/MstUserMapper.java
@@ -0,0 +1,15 @@
+package jp.co.internous.ecsite.model.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import jp.co.internous.ecsite.model.domain.MstUser;
+import jp.co.internous.ecsite.model.form.LoginForm;
+
+@Mapper
+public interface MstUserMapper {
+	
+	@Select(value="SELECT * from mst_user where user_name = #{userName} AND password = #{password}")
+	MstUser findByUserNameAndPassword(LoginForm form);
+
+}
